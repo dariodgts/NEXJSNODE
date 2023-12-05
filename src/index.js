118824d24b6c5c46c535165397d9d1a68b7d9e84
dariodgts <dariogregoriotejeda@gmail.com>
2023-12-05 15:08:06 -0400

    primer commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+//console.log("hola mundo");
+
+//const express= require("express");
+import express from 'express'
+import { dirname, join } from 'path' /*para poder usar la ruta para el frontend mostrar las vistas*/
+import {fileURLToPath} from "url" /*para obtener la direcion de nuestra vista del frontend exacta o completa*/
+import ejs from "ejs"
+
+
+const app = express()
+
+const __dirname= dirname(fileURLToPath(import.meta.url))/*para obtener la ruta de la carpeta del frontend donde esta nuestra vista*/
+
+app.set("views", join(__dirname, "views"))/*para que busque la carpeta de la vista del frontend*/
+app.set("viem engine", "ejs")
+
+//mostrando las vistas de las paginas recargadas
+app.get("/", (req, res)=>res.render("index.ejs",{inicio:"inicio"}))/*para que me llegue a la ruta del index desde el puerto 5000 */
+app.get("/inicio", (req, res)=>res.render("index.ejs",{inicio:"inicio"}))    /* enviando archivos del frontend al backend y navegar en las paginas ejs desde web */
+app.get("/acerca", (req, res)=>res.render("acerca.ejs",{acerca:"acerca"}))
+app.get("/servicios", (req, res)=>res.render("servicios.ejs",{servicios:"servicios"}))
+app.get("/contacto", (req, res)=>res.render("contacto.ejs",{contacto:"contacto"}))
+
+app.set("vista css", "css")
+app.get("/views", (req, res)=>res.render("styles.css")
+ ) 
+ 
+//para aplicarle estilos css
+ function getRenderVariables (req) {
+    const styles = fs.readFileSync(path.resolve(__dirname, './styles.css'), 'utf8')
+  
+    return {
+      styles
+      
+    }
+  }
+  
+  app.get('/mypage', (req, res) => {
+    res.render('index', getRenderVariables(req))
+  })
+
+app.listen(5000)
+
+console.log("el servidor ha respondido su peticion en el puerto",5000)
+console.log("GRACIAS POR UTILIZAR NUESTRO SERVICIO")
+
+//var express = require('express');
+//var app = express();
+
+
